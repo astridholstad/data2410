68605c2fcfbef8ac4842c1c4ca4f77e4f3015e60
astridholstad <astridholstad21@gmail.com>
2025-03-06 11:39:44 +0100

    cmts

--- Oblig2/server.py
+++ Oblig2/server.py
@@ -1,1 +1,39 @@
 from socket import *
+
+#i will develop a server that handles one http req at a time
+#the web server should accept and parse a http req 
+#get the requested file from the server’s file system
+# create an HTTP response message consisting of the requested file preceded by header line
+# and then send the response directly to the client
+# if the requested file is not present in the server, the server should sendan HTTP “404 Not Found” message back to the client
+
+#def main()
+    #create a socket
+
+""""
+Description:
+This function takes a port and IP addresses from a user
+and checks if the port and IP addresses are in the correct format
+Arguments:
+ip: holds the ip address of the server
+port: port number of the server
+Returns: true or false
+"""
+#create a tcp socket with SOCK_STREAM
+	#server_sd = socket(AF_INET, SOCK_STREAM)
+    #port = 12000
+	#server_ip ='127.0.0.1'
+
+
+    #server_sd.bind((server_ip, port)) 
+
+
+
+    #server_sd.close()
+
+
+
+
+
+#if __name__=='__main__':
+#	main()"
--- Oblig2/server_multi.py
+++ Oblig2/server_multi.py
@@ -1,2 +1,1 @@
 from socket import *
-
